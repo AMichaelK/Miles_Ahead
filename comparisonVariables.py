d63cfdcd16e237d23ad1b8f0ea2575e0605f90e6
Anthony Kan <amkan18@gmail.com>
2025-06-13 17:35:42 -0400

    Changed formatting in comparisons

--- comparisonVariables.py
+++ comparisonVariables.py
@@ -2,23 +2,23 @@ class Comparisons:
     
     # Distances
     DISTANCE_TUPLE = [
-        ("THE_GREAT_TRAIL", 16777),
-        ("AMERICAN_DISCOVERY_TRAIL", 6800),
-        ("HOKKAIDO_NATURE_TRAIL", 2848),
-        ("THE_COASTAL_PATH", 2795),
-        ("HIGHWAY_1", 9000),
-        ("TRANS_CANADA_HIGHWAY", 5000),
-        ("BOSTON_TO_OREGON", 3365),
-        ("ISTANBUL_TO_TOKYO",  12774)
+        ("The Great Trail", 16777),
+        ("American Discovery Trail", 6800),
+        ("Hokkaido Nature Trail", 2848),
+        ("The Coastal Path", 2795),
+        ("Highway 1", 9000),
+        ("Trans Canada Highway", 5000),
+        ("Boston to Oregon", 3365),
+        ("Istanbul to Tokyo", 12774)
     ]
     # Heights
     HEIGHTS_TUPLE = [
-        ("DENALI", 20308),
-        ("KILIMANJARO", 19341),
-        ("EVEREST", 29032),
-        ("MONT_BLANC", 15766),
+        ("Denali", 20308),
+        ("Kilimanjaro", 19341),
+        ("Everest", 29032),
+        ("Mont Blanc", 15766),
         ("K2", 28251),
-        ("ACONCAGUA", 22841),
-        ("RAINIER", 14409),
-        ("FUJI", 12388)
+        ("Aconcagua", 22841),
+        ("Rainier", 14409),
+        ("Fuji", 12388)
     ]
